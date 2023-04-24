cc52933fcc4b25a3abcb1221c524ecfc568c8144
nklhtv <nklhtv@gmail.com>
2023-04-24 17:30:14 +0300

    sourcemap added to the output

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   build: {
     outDir: "./dist",
     emptyOutDir: false,
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "GhostHelper",
