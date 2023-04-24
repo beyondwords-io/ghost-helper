143cf0b282c71741916acf92f2deed1746cf5dce
nklhtv <nklhtv@gmail.com>
2023-04-24 17:25:10 +0300

    rename the output file based on format

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "GhostHelper",
-      fileName: "ghost-helper",
+      fileName: format => `${format}.js`,
       formats: ["umd"],
     },
   }
