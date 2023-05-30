4b98c2853b79254069a4aeb6523560ef5ae39840
nklhtv <nklhtv@gmail.com>
2023-05-30 11:05:46 +0300

    update player

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import BeyondWords from "@beyondwords/player";
+import "@beyondwords/player";
 import GhostHelper from "./GhostHelper";
 
-BeyondWords.GhostHelper = GhostHelper;
+if (typeof window !== "undefined") {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any).BeyondWords ||= {};
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (window as any).BeyondWords.GhostHelper = GhostHelper;
+}
 
 export default GhostHelper;
