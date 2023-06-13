c771773aee53118548594f2090499d0568237f42
nklhtv <nklhtv@gmail.com>
2023-06-13 13:32:55 +0300

    remove the filtering of style/script elements

--- src/assignMarkers.ts
+++ src/assignMarkers.ts
@@ -1,5 +1,5 @@
 export default function assignMarkers(content: Element) {
   Array.from(content.children)
-    .filter(child => child instanceof HTMLElement && child.tagName !== "STYLE" && child.tagName !== "SCRIPT" && typeof child.dataset.beyondwordsMarker !== "string")
+    .filter(child => child instanceof HTMLElement && typeof child.dataset.beyondwordsMarker !== "string")
     .forEach((child, index) => (child as HTMLElement).dataset.beyondwordsMarker = `data-segment-${index}`);
 }
