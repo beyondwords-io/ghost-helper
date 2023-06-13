c6bdef104a6151644c9c1c50cadfa4f39ad986a2
nklhtv <nklhtv@gmail.com>
2023-06-13 13:53:52 +0300

    move the marker name into a const

--- src/assignMarkers.ts
+++ src/assignMarkers.ts
@@ -1,5 +1,7 @@
+const BW_MARKER_DATASET_NAME = "beyondwordsMarker";
+
 export default function assignMarkers(contentTargetElement: Element) {
   Array.from(contentTargetElement.children)
-    .filter(child => child instanceof HTMLElement && typeof child.dataset.beyondwordsMarker !== "string")
-    .forEach((child, index) => (child as HTMLElement).dataset.beyondwordsMarker = `segment-${index}`);
+    .filter(child => child instanceof HTMLElement && typeof child.dataset[BW_MARKER_DATASET_NAME] !== "string")
+    .forEach((child, index) => (child as HTMLElement).dataset[BW_MARKER_DATASET_NAME] = `segment-${index}`);
 }
