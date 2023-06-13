2a3e0a15b06dbb11743d3ce5fe677fb414711f03
nklhtv <nklhtv@gmail.com>
2023-06-13 13:39:00 +0300

    remove the data prefix from the marker value

--- src/assignMarkers.ts
+++ src/assignMarkers.ts
@@ -1,5 +1,5 @@
 export default function assignMarkers(content: Element) {
   Array.from(content.children)
     .filter(child => child instanceof HTMLElement && typeof child.dataset.beyondwordsMarker !== "string")
-    .forEach((child, index) => (child as HTMLElement).dataset.beyondwordsMarker = `data-segment-${index}`);
+    .forEach((child, index) => (child as HTMLElement).dataset.beyondwordsMarker = `segment-${index}`);
 }
