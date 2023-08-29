7b2d185e3eebc7b2c5396ae31c809be3134bbeb0
nklhtv <nklhtv@gmail.com>
2023-08-29 17:54:47 +0300

    assign GhostHelper to the global scope only if its not being assigned already

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ if (typeof window !== "undefined") {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).BeyondWords ||= {};
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (window as any).BeyondWords.GhostHelper = GhostHelper;
+  (window as any).BeyondWords.GhostHelper ||= GhostHelper;
 }
 
 export default GhostHelper;
